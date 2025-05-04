a961269ecf066b01079686724a8a734177de733c
abitmarty <matijn.totte@gmail.com>
2025-05-04 17:35:05 +0200

    Added add more player instruction

--- screens/AddPlayersScreen.js
+++ screens/AddPlayersScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useRef, useState, useEffect } from "react";
-import { SafeAreaView, Text, TextInput, View, FlatList, StyleSheet, Pressable } from "react-native";
+import { View, FlatList, StyleSheet, Animated } from "react-native";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import { GameContext } from "../store/context/GameContext";
 import GameBackground from "../components/ui/GameBackground";
@@ -8,6 +8,8 @@ import InputCard from "../components/ui/InputCard";
 import Colors from '../constants/colors';
 import PlayerCard from "../components/ui/PlayerCard";
 import { useFocusEffect } from '@react-navigation/native'; // Import useFocusEffect
+import FontSize from "../constants/FontSize";
+import TextCustom from "../components/ui/TextCustom";
 
 function AddPlayersScreen({ navigation }) {
   const [playerName, setPlayerName] = useState("");
@@ -22,8 +24,6 @@ function AddPlayersScreen({ navigation }) {
 
   const removePlayerHandler = (playerId) => {
     removePlayer(playerId)
-    // console.log(playerId)
-    // console.log(players)
   }
 
   useEffect(() => {
@@ -38,31 +38,51 @@ function AddPlayersScreen({ navigation }) {
       functionCall = () => { addPlayerHandler(); navigation.navigate("SetRoundsScreen")};
   }
 
+  const playersNeeded = 3 - players.length;
+  let footerTopText = "You need 1 more player!"
+  if (playersNeeded > 1){
+    footerTopText = `You need ${playersNeeded} more players!`;
+  }
+
+  const slideAnim = useRef(new Animated.Value(50)).current; // hidden by default
+
+  useEffect(() => {
+    Animated.timing(slideAnim, {
+      toValue: players.length < 3 ? 0 : 50, // show when <3, hide when ≥3
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  }, [players.length]);
+
+
   return (
     <GameBackground>
-        <SmallButton onPress={() => navigation.popTo('Home')}/>
-        <View style={styles.mainContainer}>
-          <InputCard
-            title="Add Players:"
-            placeholder="Player name"
-            value={playerName}
-            onChangeText={setPlayerName}
-            onSubmitEditing={addPlayerHandler}
-          />
-          <FlatList
-            data={players}
-            numColumns={2}
-            keyExtractor={(item, index) => index.toString()}
-            style={styles.flat}
-            renderItem={({ item }) => (
-              <View style={styles.cardContainer}>
-                <PlayerCard onPress={() => removePlayerHandler(item.id)}>{item.name}</PlayerCard>
-              </View>
-            )}
-            contentContainerStyle={styles.flatListContent}
-            columnWrapperStyle={styles.row}
-          />
-        </View>
+      <SmallButton onPress={() => navigation.popTo('Home')}/>
+      <View style={styles.mainContainer}>
+        <InputCard
+          title="Add Players:"
+          placeholder="Player name"
+          value={playerName}
+          onChangeText={setPlayerName}
+          onSubmitEditing={addPlayerHandler}
+        />
+        <FlatList
+          data={players}
+          numColumns={2}
+          keyExtractor={(item, index) => index.toString()}
+          style={styles.flat}
+          renderItem={({ item }) => (
+            <View style={styles.cardContainer}>
+              <PlayerCard onPress={() => removePlayerHandler(item.id)}>{item.name}</PlayerCard>
+            </View>
+          )}
+          contentContainerStyle={styles.flatListContent}
+          columnWrapperStyle={styles.row}
+        />
+      </View>
+      <Animated.View style={[styles.footerTop, { transform: [{ translateY: slideAnim }] }]}>
+        <TextCustom style={styles.footerText}>{footerTopText}</TextCustom>
+      </Animated.View>
       <View style={styles.footer}>
         <PrimaryButton disabled={buttonDisabled} onPress={functionCall}>Continue</PrimaryButton>
         <PrimaryButton onPress={addPlayerHandler} typeBtn="add" ></PrimaryButton>
@@ -105,6 +125,19 @@ const styles = StyleSheet.create({
       left: 0,
       right: 0,
       gap: 14,
+      zIndex: 2,
+    },
+    footerTop: {
+      position: 'absolute',
+      bottom: 100, // just above the footer
+      zIndex: 0,
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    footerText: {
+      color: 'white',
+      fontSize: FontSize.mid,
     },
     title: {
       fontSize: 20,
